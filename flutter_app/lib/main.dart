30285ff4595fd9a3569511b204a566989ac28314
Oleh <oleh.m97@gmail.com>
2021-08-22 21:57:17 +0300

    hjk

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -6,13 +6,20 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Dart App', style: TextStyle(
-        fontSize: 20,
-        color: Colors.blueAccent,
-        fontFamily: 'Times New Ran'
-      ),)
+      theme: ThemeData(primaryColor: Colors.lightBlue),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Dart App'),
+          ),
+          body: Text(
+            'Dart App',
+            style: TextStyle(
+                fontSize: 20,
+                color: Colors.blueAccent,
+                fontFamily: 'Times New Ran'),
+          )),
     );
   }
-  
+
 }
 
